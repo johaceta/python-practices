71eb984ea9a2cd8bcf1711731f6bf922b111e46c
johaceta <johaceta@gmail.com>
2024-09-27 15:24:31 -0500

    Update multipleofanumber.py
    Updated function to start in 1 and include all number in n

--- multipleofanumber.py
+++ multipleofanumber.py
@@ -15,7 +15,7 @@ import sys
 #
 
 def fizzBuzz(n):
-    for i in range(n):
+    for i in range(1, n+1):
         if (i % 3 == 0) and (i % 5 == 0):
             print('FizzBuzz')
         elif (i % 3 == 0) and (i % 5 != 0):
